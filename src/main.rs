2fd1b53b4aa3573f47d0bf0927a614156b9a01e4
Douglas H. Bradshaw <dhbradshaw@gmail.com>
2017-07-11 17:00:28 -0500

    Euler 1 without looking at any documentation :-D

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+fn p1(bar: u32) -> u32 {
+    println!("If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.");
+    let mut n = 1;
+    let mut agg = 0;
+    loop {
+        if n == bar {
+            break;
+        }
+        if n % 3 == 0 || n % 5 == 0 {
+            agg += n;
+        }
+        n += 1;
+    }
+    agg
+}
+
+fn main() {
+    println!("{}",p1(10));
+    println!("{}",p1(1000));
+}
