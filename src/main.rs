1a2cdccf9cc792b79c8823e17e11a1144d3c05fa
Douglas H. Bradshaw <dhbradshaw@gmail.com>
2017-07-12 10:14:34 -0500

    Add iterative method for p1.  (It's three times slower for large numbers.)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 fn p1(bar: u64) -> u64 {
-    println!("If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.");
+    /// If we list all the natural numbers below 10 that are multiples of 3 or 5,
+    /// we get 3, 5, 6 and 9. The sum of these multiples is 23.
+    /// Find the sum of all the multiples of 3 or 5 below 1000.");
     let mut n = 1;
     let mut agg = 0;
     loop {
@@ -20,7 +21,18 @@ fn test_p1() {
     assert!(p1(10) == 23)
 }
 
+fn p1_iterate(bar: u64) -> u64 {
+    (1..bar).filter(|n| n % 3 == 0 || n % 5 == 0).sum()
+}
+
+#[test]
+fn test_p1_iterate() {
+    assert!(p1_iterate(10) == 23)
+}
+
 fn main() {
     println!("{}", p1(10));
+    println!("{}", p1_iterate(10));
     println!("{}", p1(1000));
+    println!("{}", p1_iterate(1000));
 }
