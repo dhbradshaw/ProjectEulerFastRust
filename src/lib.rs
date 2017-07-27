39dd2c3c2c86b36b978a329c9a08fb877a1fe5a9
Douglas H. Bradshaw <dhbradshaw@gmail.com>
2017-07-27 08:18:49 -0500

    Forget the struct stuff.  Let's get the algorithm right.

--- src/lib.rs
+++ src/lib.rs
@@ -1,144 +1,34 @@
 use std::cmp::max;
 use std::cmp::min;
 
-#[derive(Debug, PartialEq)]
-pub struct IJ {
-    pub i: usize,
-    pub j: usize
-}
-impl IJ {
-    pub fn new(i: usize, j: usize) -> IJ {
-        IJ{i, j}
-    }
-    pub fn clone(&self) -> IJ {
-        IJ::new(self.i, self.j)
-    }
-    pub fn down(&self) -> IJ {
-        IJ::new(self.i + 1, self.j)
-    }
-    pub fn right(&self) -> IJ {
-        IJ::new(self.i, self.j + 1)
-    }
-    pub fn ascend(&self) -> IJ {
-        IJ::new(self.i - 1, self.j + 1)
-    }
-    pub fn descend(&self) -> IJ {
-        IJ::new(self.i + 1, self.j + 1)
-    }
-}
 
-pub struct AbstractSquareMatrix {
-    pub order: usize
-}
 
-impl AbstractSquareMatrix {
-    fn rows(&self) -> Vec<Vec<IJ>> {
-        (0..self.order).map(|i| {
-            (0..self.order).map(|j|{
-                IJ{i, j}
-            }).collect()
-        }).collect()
-    }
-    fn columns(&self) -> Vec<Vec<IJ>> {
-        (0..self.order).map(|i| {
-            (0..self.order).map(|j|{
-                IJ{i:j, j:i}
-            }).collect()
-        }).collect()
-    }
-    fn climbs(&self) -> Vec<Vec<IJ>> {
-        let out = self.order;
-        let scan_count: usize = 2 * out - 1;
-        let mut first = IJ::new(0,0);
-        let mut scans = Vec::new();
-        for scan in 0..scan_count {
-            println!("{:?}", first);
-            let mut ij = first.clone();
-            let mut scan_vec = vec![ij.clone()];
-            while ij.j < (out - 1) {
-                ij = ij.ascend();
-                println!("{:?}", ij);
-                scan_vec.push(ij.clone());
-            }
-            if first.i < (out - 1) {
-                first = first.down();
-            } else {
-                first = first.right();
-            }
+fn climb(order: usize, sum: usize) -> Vec<(usize, usize)> {
+    let largest = order - 1;
+    let smallest = 0;
+
+    let mut i = min(largest, sum);
+    let mut j = sum - i;
+    let mut v = Vec::new();
+    loop {
+        v.push((i, j));
+        if i <= smallest || j >= largest {
+            break;
         }
-        return scans
+        i = i - 1;
+        j = j + 1;
     }
+    v
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
     #[test]
-    fn rows_2() {
-        let asm = AbstractSquareMatrix{order:2};
-        let rows = asm.rows();
-        assert_eq!(
-            rows.len(),
-            2
-        );
-        assert_eq!(
-            rows[0].len(),
-            2
-        );
-        assert_eq!(rows[0][0], IJ{i: 0, j: 0});
-        assert_eq!(rows[0][1], IJ{i: 0, j: 1});
-    }
-    #[test]
-    fn cols_2() {
-        let asm = AbstractSquareMatrix{order:2};
-        let cols = asm.columns();
-        assert_eq!(
-            cols.len(),
-            2
-        );
-        assert_eq!(
-            cols[0].len(),
-            2
-        );
-        assert_eq!(cols[0][0], IJ{i: 0, j: 0});
-        assert_eq!(cols[0][1], IJ{i: 1, j: 0});
-    }
-    // #[test]
-    // fn climbs_1() {
-    //     let asm = AbstractSquareMatrix{order: 1};
-    //     let climbs = asm.climbs();
-    //     for scan in climbs {
-    //         for item in scan {
-    //             println!("i: {}, j: {}", item.i, item.j);
-    //         }
-    //         println!();
-    //     }
-    // }
-    // #[test]
-    // fn climbs_2() {
-    //     let asm = AbstractSquareMatrix{order: 2};
-    //     let climbs = asm.climbs();
-    //     for scan in climbs {
-    //         for item in scan {
-    //             println!("i: {}, j: {}", item.i, item.j);
-    //         }
-    //         println!();
-    //     }
-    // }
-    // #[test]
-    // fn climbs_3() {
-    //     let asm = AbstractSquareMatrix{order: 3};
-    //     let climbs = asm.climbs();
-    //     for scan in climbs {
-    //         for item in scan {
-    //             println!("i: {}, j: {}", item.i, item.j);
-    //         }
-    //         println!();
-    //     }
-    // }
-    #[test]
-    fn climbs_3() {
-        let asm = AbstractSquareMatrix{order: 3};
-        asm.climbs();
+    fn test_climb_first() {
+        assert_eq!(climb(1, 0), vec![(0, 0)]);
+        assert_eq!(climb(2, 0), vec![(0, 0)]);
+        assert_eq!(climb(2, 1), vec![(1, 0), (0, 1)]);
+        assert_eq!(climb(2, 2), vec![(1, 1)]);
     }
 }
