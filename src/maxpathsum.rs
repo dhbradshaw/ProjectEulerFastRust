59a7a1cd834c977f588eeb54dcbde7f2d11006e8
Douglas H. Bradshaw <dhbradshaw@gmail.com>
2017-08-09 07:58:04 -0500

    Wicked fast algorithm implemented in a wicked fast way.

--- src/maxpathsum.rs
+++ src/maxpathsum.rs
@@ -1,5 +1,13 @@
-pub fn one() -> u64 {
-    1
+use std::cmp::max;
+
+pub fn highest_values(upper: &Vec<u32>, lower: &Vec<u32>) -> Vec<u32> {
+    let l = upper.len();
+    let mut v = Vec::new();
+    for i in 0..l {
+        let agg = max(lower[i], lower[i+1]) + upper[i];
+        v.push(agg)
+    }
+    v
 }
 
 
@@ -7,8 +15,20 @@ pub fn one() -> u64 {
 mod test {
     use super::*;
     #[test]
-    fn dummy_test() {
-        assert_eq!(one(), 1);
-
+    fn test_highest_values() {
+        assert_eq!(
+            highest_values(
+                &vec![1],
+                &vec![1, 2]
+            ),
+            vec![3]
+        );
+        assert_eq!(
+            highest_values(
+                &vec![3, 2, 1],
+                &vec![7, 20, 57, 46]
+            ),
+            vec![23, 59, 58]
+        );
     }
 }
